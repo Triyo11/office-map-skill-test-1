1a2653f66f6e5ecacea3a881578913bab293102a
Triyo Krismantoro <rusdiantoro117@gmail.com>
2025-02-13 13:02:56 +0700

    dev: create custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      black: "#1E293B",
+      red: "#EF4444",
+      white: "#FFFFFF",
+      transparent: "transparent",
+    },
   },
   plugins: [],
 };
